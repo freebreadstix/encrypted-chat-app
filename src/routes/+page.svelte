15b5113b45087789ac2eb519343c6b56bfa46a83
freebreadstix <lnguyen.professional@gmail.com>
2023-02-05 02:33:18 -0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	let message = '';
+	const sendMessage = () => {
+		if (message !== '') {
+			console.log(message);
+			message = '';
+		}
+	};
+</script>
+
+<!-- <label>, name, id -->
+<input bind:value={message} />
+<button on:click={sendMessage}>Send</button>
