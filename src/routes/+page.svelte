f24be7903d1f16789623615edb6c6a9d469e505a
freebreadstix <lnguyen.professional@gmail.com>
2023-02-05 04:11:47 -0800

    Chat to self, conn supabase, add tailwind. daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,47 @@
 <script>
-	let message = '';
-	const sendMessage = () => {
-		if (message !== '') {
-			console.log(message);
-			message = '';
+	import { supabase } from '$lib/db';
+	import { onMount } from 'svelte';
+
+	let messages = [];
+	const TABLE_NAME = import.meta.env.VITE_SUPABASE_TABLE_NAME;
+
+	onMount(() => {
+		fetchMessages();
+	});
+
+	let messageToSend = '';
+
+	const fetchMessages = async () => {
+		let { data, error } = await supabase.from(TABLE_NAME).select('*');
+		if (error) {
+			console.log('error', error);
+		} else {
+			messages = data;
+			console.log(messages);
+		}
+	};
+
+	const sendMessage = async () => {
+		if (messageToSend !== '') {
+			let { data: message, error } = await supabase
+				.from(TABLE_NAME)
+				.insert({ message: messageToSend, sender: 'freebreadstix' })
+				.select()
+				.single();
+			if (error) {
+				console.log(error.message);
+			} else {
+				fetchMessages();
+				console.log(messageToSend);
+				messageToSend = '';
+			}
 		}
 	};
 </script>
 
+{#each messages as messageObject}
+	<li>{messageObject.sender}: {messageObject.message}</li>
+{/each}
 <!-- <label>, name, id -->
-<input bind:value={message} />
-<button on:click={sendMessage}>Send</button>
+<input bind:value={messageToSend} />
+<button class="btn btn-primary" on:click={sendMessage}>Send</button>
